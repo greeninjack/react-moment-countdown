e88e0ac70f6cade1fa26e7978bb066a90acc5257
jordanhudgens <jordan@devcamp.com>
2019-10-09 12:20:41 -0700

    Updated the return type for the date so that a string is returned instead of a span

--- src/index.jsx
+++ src/index.jsx
@@ -1,72 +1,76 @@
-import React, { Component } from 'react'
-import { oneOfType, func, instanceOf, string } from 'prop-types'
-import { momentObj } from 'react-moment-proptypes'
+import React, { Component } from "react";
+import { oneOfType, func, instanceOf, string } from "prop-types";
+import { momentObj } from "react-moment-proptypes";
 
-import formatDate from './format-date'
+import formatDate from "./format-date";
 
 class ReactMomentCountDown extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       countdown: null
-    }
+    };
   }
 
   componentDidMount() {
-    this.tick()
+    this.tick();
 
-    this.timer = window.setInterval(this.tick.bind(this), 1000)
+    this.timer = window.setInterval(this.tick.bind(this), 1000);
   }
 
   componentWillUnmount() {
-    window.clearInterval(this.timer)
+    window.clearInterval(this.timer);
   }
 
   tick() {
-    const { toDate, sourceFormatMask, targetFormatMask, onCountdownEnd, onTick } = this.props
-    const [delta, countdown] = formatDate(toDate, targetFormatMask, sourceFormatMask)
+    const {
+      toDate,
+      sourceFormatMask,
+      targetFormatMask,
+      onCountdownEnd,
+      onTick
+    } = this.props;
+    const [delta, countdown] = formatDate(
+      toDate,
+      targetFormatMask,
+      sourceFormatMask
+    );
 
     if (delta <= 0) {
-      window.clearInterval(this.timer)
+      window.clearInterval(this.timer);
 
       if (onCountdownEnd) {
-        onCountdownEnd()
+        onCountdownEnd();
       }
     }
 
     this.setState({
       countdown
-    })
+    });
 
     if (onTick) {
-      onTick(delta)
+      onTick(delta);
     }
   }
   render() {
-    return (
-      <span>{this.state.countdown}</span>
-    )
+    return this.state.countdown;
   }
-};
+}
 
 ReactMomentCountDown.propTypes = {
-  toDate: oneOfType([
-    momentObj,
-    instanceOf(Date),
-    string
-  ]).isRequired,
+  toDate: oneOfType([momentObj, instanceOf(Date), string]).isRequired,
   sourceFormatMask: string,
   targetFormatMask: string,
   onTick: func,
   onCountdownEnd: func
-}
+};
 
 ReactMomentCountDown.defaultProps = {
-  sourceFormatMask: 'YYYY-MM-DD',
-  targetFormatMask: 'HH:mm:ss',
+  sourceFormatMask: "YYYY-MM-DD",
+  targetFormatMask: "HH:mm:ss",
   onTick: null,
   onCountdownEnd: null
-}
+};
 
-export default ReactMomentCountDown
+export default ReactMomentCountDown;
